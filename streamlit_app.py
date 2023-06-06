32e26e53b5ca1456794f2ac205c7b8df55625fdd
willhjackson <135681676+willhjackson@users.noreply.github.com>
2023-06-06 15:53:16 +1200

    Update streamlit_app.py
    added fruits to show in code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick lsit here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_Show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
